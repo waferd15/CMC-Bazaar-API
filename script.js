83beb2c9a3a566f48bfc29b489b3b8a4853b361f
waferd15 <wasifahmed1221@gmail.com>
2026-05-25 15:33:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("this page works")
+fetch("https://api.craftersmc.net/v1/skyblock/bazaar/enchanted_melon_block/details", {
+  headers: {
+    "X-API-Key": "YOUR_KEY"
+  }
+})
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+});
